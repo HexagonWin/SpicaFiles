f299218dd02cc4db2889c74613bfba5d1f0b74d4
HexWin <baickainetwork@gmail.com>
2020-08-18 13:48:01 +0900

    s

--- daum/_makeindex.sh
+++ daum/_makeindex.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ls | egrep '(jpg|png|apk)' | \\ #filter the content you want in the index
+ls | egrep '(jpg|png|apk)' |  #filter the content you want in the index
 perl -e 'print "<html><body><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
